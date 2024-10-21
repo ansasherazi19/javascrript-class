190014f4a157c047ba16ed1d8f8048bf5c0685d2
Ansa-sherazi <155802749+Ansa-sherazi@users.noreply.github.com>
2024-10-21 19:02:38 +0500

    Add files via upload

---
+++ New folder/app.js
@@ -0,0 +1,21 @@
+console.log("hello class")
+// ALERT
+window.alert("what")
+alert("kiya matalb")
+//  CONSOLE DEBUGGER
+console.log("hello world")
+//   Document.WRITE
+document.write("wellcome" +"<br>")
+document.write(60+567)
+
+
+var name="Ansa sherazi";
+var isvalid=true;
+var year=2024;
+console.log(typeof(name));//string
+console.log(typeof(isvalid));//boolean
+console.log(typeof(year)); //number
+
+var firstname;
+firstname="ansa";
+document.write(firstname)
