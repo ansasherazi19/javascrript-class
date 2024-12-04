dd180cbe82d106a6831a30670dd554433d927f46
Ansa-sherazi <155802749+Ansa-sherazi@users.noreply.github.com>
2024-12-04 19:00:56 +0500

    add

---
+++ cart/cart.js
@@ -0,0 +1,42 @@
+let products = [
+    {
+      id: 1,
+      img: "img1.avif",
+      title: "Gaming Headphone 1",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, autem.",
+      price: 3999,
+      off: 20,
+    },
+    {
+      id: 2,
+      img: "img2.jpg",
+      title: "Gaming Headphone 2",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, autem.",
+      price: 4500,
+      off: 80,
+    },
+    {
+      id: 3,
+      img: "img3.jpg",
+      title: "Gaming Headphone 3",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, autem.",
+      price: 2500,
+      off: 11,
+    },
+    {
+      id: 4,
+      img: "img4.avif",
+      title: "Gaming Headphone 4",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, autem.",
+      price: 1500,
+      off: 60,
+    },
+    {
+      id: 5,
+      img: "img4.avif",
+      title: "Gaming Headphone 5",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, autem.",
+      price: 1000,
+      off: 30,
+    },
+  ];
